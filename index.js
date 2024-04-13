7c68e0ef2a23dcf0eb508e22eba6fc9188224c3c
Asim74 <asimr6573@gmail.com>
2024-04-13 11:32:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+async function main() {
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-6: ",
+        },
+    ]);
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    console.log("Computer's number:", randomNumber);
+    if (answer.userGuessedNumber === randomNumber) {
+        console.log("Congratulations! You guessed the right number.");
+    }
+    else {
+        console.log("Sorry, you guessed the wrong number.");
+    }
+}
+main().catch(error => {
+    console.error(error);
+});
